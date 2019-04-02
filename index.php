7fc0323f6d140e2a5d160f3e72ab7ff454a8b8ea
simoneneves2020 <42121742018.1@senacrjedu.local>
2019-04-02 10:49:55 -0300

    pagina inicial

---
+++ index.php
@@ -0,0 +1,23 @@
+.<!DOCTYPE html>
+<html>
+<head>
+	<title>Aula3 Git</title>
+</head>
+<body>
+	<h1>Cadastro de Cliente</h1>
+	<form>
+		Nome:<br>
+		<input type="text" name="nome"/>
+		<br>
+		Email<br>
+		<input type="text" name="email"/>
+		<br>
+		Telefone<br>
+		<input type="text" name="telefone"/>
+		<br>
+		<input type="submit" value="Gravar"/>
+		<input type="reset" value="Limpar Campos"/>
+
+	</form>
+</body>
+</html>
